dc21080dee1a658604450be19f717f78a35f513e
stearm210 <pyzxWoo@163.com>
2024-12-15 15:34:20 +0800

    点位信息的改造

--- dkd-manage/src/main/java/com/dkd/manage/service/INodeService.java
+++ dkd-manage/src/main/java/com/dkd/manage/service/INodeService.java
@@ -2,6 +2,7 @@ package com.dkd.manage.service;
 
 import java.util.List;
 import com.dkd.manage.domain.Node;
+import com.dkd.manage.domain.vo.NodeVo;
 
 /**
  * 点位管理Service接口
@@ -58,4 +59,10 @@ public interface INodeService
      * @return 结果
      */
     public int deleteNodeById(Long id);
+
+
+    /*
+    * 查询点位列表的方法
+    * */
+   public List<NodeVo> selectNodeVoList(Node node);
 }
