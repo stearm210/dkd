17f5a3c346523a19e6ec5f34611341bc4d2edc3f
stearm210 <pyzxWoo@163.com>
2024-12-20 16:09:47 +0800

    设备管理

--- dkd-manage/src/main/java/com/dkd/manage/service/impl/VendingMachineServiceImpl.java
+++ dkd-manage/src/main/java/com/dkd/manage/service/impl/VendingMachineServiceImpl.java
@@ -1,12 +1,20 @@
 package com.dkd.manage.service.impl;
 
 import java.util.List;
+
+import cn.hutool.core.bean.BeanUtil;
 import com.dkd.common.utils.DateUtils;
+import com.dkd.common.utils.uuid.UUIDUtils;
+import com.dkd.manage.domain.Node;
+import com.dkd.manage.domain.VmType;
+import com.dkd.manage.service.INodeService;
+import com.dkd.manage.service.IVmTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.dkd.manage.mapper.VendingMachineMapper;
 import com.dkd.manage.domain.VendingMachine;
 import com.dkd.manage.service.IVendingMachineService;
+
 
 /**
  * 设备管理Service业务层处理
@@ -19,6 +27,12 @@ public class VendingMachineServiceImpl implements IVendingMachineService
 {
     @Autowired
     private VendingMachineMapper vendingMachineMapper;
+
+    @Autowired
+    private IVmTypeService vmTypeService;
+
+    @Autowired
+    private INodeService nodeService;
 
     /**
      * 查询设备管理
@@ -46,15 +60,33 @@ public class VendingMachineServiceImpl implements IVendingMachineService
 
     /**
      * 新增设备管理
-     * 
+     * 需要将属性从一张表中拷贝到另一张表中
      * @param vendingMachine 设备管理
      * @return 结果
      */
     @Override
     public int insertVendingMachine(VendingMachine vendingMachine)
     {
+        // 需要将属性从一张表中拷贝到另一张表中
+        //1.新增设备
+        //1-1 随机生成8位唯一标识，补充货道编号
+        String innerCode = UUIDUtils.getUUID();
+        vendingMachine.setInnerCode(innerCode);
+        //1-2 查询售货机类型表，补充设备容量
+        VmType vmType = vmTypeService.selectVmTypeById(vendingMachine.getVmTypeId());//查到对应设备
+        vendingMachine.setChannelMaxCapacity(vmType.getChannelMaxCapacity());//设置对应容量
+        //1-3 查询点位表，补充区域、点位、合作商等信息
+        Node node = nodeService.selectNodeById(vendingMachine.getNodeId());
+        //属性快速拷贝
+        BeanUtil.copyProperties(node,vendingMachine,"id");
+
+
         vendingMachine.setCreateTime(DateUtils.getNowDate());
-        return vendingMachineMapper.insertVendingMachine(vendingMachine);
+        int result = vendingMachineMapper.insertVendingMachine(vendingMachine);
+        //2.新增货道
+
+
+        return result;
     }
 
     /**
