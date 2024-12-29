4e1d7a600be29c528537c084e5fb3abda3cd30b4
stearm210 <pyzxWoo@163.com>
2024-12-29 15:58:29 +0800

    查看详情改造

--- dkd-manage/src/main/java/com/dkd/manage/controller/TaskDetailsController.java
+++ dkd-manage/src/main/java/com/dkd/manage/controller/TaskDetailsController.java
@@ -101,4 +101,20 @@ public class TaskDetailsController extends BaseController
     {
         return toAjax(taskDetailsService.deleteTaskDetailsByDetailsIds(detailsIds));
     }
+    
+     /*
+      * @Title:
+      * @Author: pyzxW
+      * @Date: 2025-12-29 15:53:57
+      * @Params:  
+      * @Return: null
+      * @Description: 查看补货详情
+      */
+     @PreAuthorize("@ss.hasPermi('manage:taskDetails:list')")
+     @GetMapping(value = "/byTaskId/{taskId}")
+    public AjaxResult byTaskId(@PathVariable("taskId") Long taskId){
+         TaskDetails taskDetails = new TaskDetails();
+         taskDetails.setTaskId(taskId);
+         return success(taskDetailsService.selectTaskDetailsList(taskDetails));
+     }
 }
